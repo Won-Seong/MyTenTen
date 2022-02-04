03c651bb36ce070f24a0e83c2bec71aac4185f04
Won-Seong <54873618+Won-Seong@users.noreply.github.com>
2022-02-04 11:43:55 +0900

    Score Update to DB

--- MyTenTen/app.py
+++ MyTenTen/app.py
@@ -3,8 +3,8 @@ This script runs the application using a development server.
 It contains the definition of routes and views for the application.
 """
 
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template, request, redirect, send_file, jsonify
+import DB_Info
 app = Flask(__name__)
 
 # Make the WSGI interface available at the top level so wfastcgi can get it.
@@ -14,6 +14,13 @@ wsgi_app = app.wsgi_app
 def hello():
     """Renders a sample page."""
     return render_template('index.html')
+
+@app.route('/DB_insert', methods = ['POST'])
+def db_insert() :
+    score = request.get_json("score")
+    DB_Info.cursor.execute(f"INSERT INTO twofourzeroeight(nickname, score) VALUES ('Lee' , {score})")
+    DB_Info.cnxn.commit()
+    return
 
 if __name__ == '__main__':
     import os
