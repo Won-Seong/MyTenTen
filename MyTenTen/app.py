5a47388444fcaa11ec098210bf8de83bd7ceea37
Won-Seong <54873618+Won-Seong@users.noreply.github.com>
2022-02-07 02:53:21 +0900

    Score Update and HTML

--- MyTenTen/app.py
+++ MyTenTen/app.py
@@ -5,26 +5,35 @@ It contains the definition of routes and views for the application.
 
 from flask import Flask, render_template, request, redirect, send_file, jsonify
 import DB_Info
+import game_page
 
 app = Flask(__name__)
+app.register_blueprint(game_page.bp)
 # Make the WSGI interface available at the top level so wfastcgi can get it.
 wsgi_app = app.wsgi_app
+nickname = "Anonymous"
 
 @app.route('/')
 def hello():
     """Renders a sample page."""
-    return render_template('index.html')
+    return render_template('index.html', nickname = nickname)
+
+@app.route('/nickname', methods = ["POST"])
+def set_nickname():
+    global nickname
+    if request.method == 'POST':
+        result = request.form["nickname"]
+        nickname = str(result)
+        print(nickname)
+    return render_template('index.html',nickname = nickname)
 
 @app.route('/DB_insert', methods = ['POST'])
 def db_insert() :
+    print(nickname)
     score = request.get_json("score")
-    DB_Info.cursor.execute(f"INSERT INTO twofourzeroeight(nickname, score) VALUES ('Lee' , {score})")
+    DB_Info.cursor.execute(f"INSERT INTO twofourzeroeight(nickname, score) VALUES ('{nickname}' , {score})")
     DB_Info.cnxn.commit()
     return 'hehe'
-
-@app.route('/TwoFourZeroEight')
-def two_four_zero_eight():
-    return render_template('two_four_zero_eight.html')
 
 if __name__ == '__main__':
     import os
--- MyTenTen/game_page.py
+++ MyTenTen/game_page.py
@@ -1,8 +1,14 @@
-#from flask import Blueprint
-#from flask import render_template
+from flask import Blueprint, render_template, request
+import DB_Info
 
-#bp = Blueprint('main', __name__, url_prefix='/')
+bp = Blueprint('main', __name__, url_prefix='/')
 
-#@bp.route('/TwoFourZeroEight')
-#def two_four_zero_eight():
-#    return render_template('two_four_zero_eight.html')
+@bp.route('/TwoFourZeroEight')
+def two_four_zero_eight():
+    return render_template('two_four_zero_eight.html')
+
+@bp.route('/TwoFourZeroEightScore')
+def two_four_zero_eight_score():
+    result = DB_Info.cursor.execute("SELECT nickname, score FROM twofourzeroeight_score_view;").fetchall()
+    return render_template('score.html', result = result, len = len(result))
+
